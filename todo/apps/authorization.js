7b056285965ef1c67b4204b38c2b94456bc17bb1
aygulyWeb <92108174+aygulyWeb@users.noreply.github.com>
2022-06-07 19:03:47 +0500

    new-change-2

--- todo/apps/authorization.js
+++ todo/apps/authorization.js
@@ -1,19 +1,75 @@
 class Authorization {
-	constructor({ input, form, login, password, button }) {
-		this.input = document.querySelectorAll(input);
+	constructor({ input, form, regFormAuth, login, password, btnSubmit, regLinkAuth,
+		createFormAcc, createPass, passwordConfirm, checkPass, btnCreate, createLinkAcc }) {
+
 		this.form = document.querySelector(form);
+		this.input = document.querySelectorAll(input);
+		this.regFormAuth = document.querySelector(regFormAuth);
 		this.login = document.querySelector(login);
 		this.password = document.querySelector(password);
-		this.button = document.querySelector(button);
+		this.btnSubmit = document.querySelector(btnSubmit);
+		this.regLinkAuth = document.querySelector(regLinkAuth);
+
+
+		this.createFormAcc = document.querySelector(createFormAcc);
+		this.createPass = document.querySelector(createPass);
+		this.passwordConfirm = document.querySelector(passwordConfirm);
+		this.checkPass = document.querySelector(checkPass);
+		this.btnCreate = document.querySelector(btnCreate);
+		this.createLinkAcc = document.querySelector(createLinkAcc);
 
 	}
 
 	init() {
-		this.createAcc();
+		this.getAuth();
 		this.validLogin();
 		this.validPassword();
+		this.createAcc();
+		this.showPassword();
+		this.getRedirectCreate();
+		this.getRedirectAcc();
+		this.getLocalStorage();
+	}
+	getLocalStorage() {
+		let obj = {
+			login: this.login.value,
+			password: this.password.value
+		}
+		let json = JSON.stringify(obj);
+
+		if (localStorage.getItem('obj') === null) {
+			localStorage.setItem('obj', json)
+		}
+		let objectReg = JSON.parse(localStorage.getItem('obj'));
+		console.log(objectReg)
+
 	}
 
+	getRedirectCreate() {
+
+		this.createFormAcc.classList.add('active');
+
+		this.regLinkAuth.addEventListener('click', (e) => {
+
+			e.preventDefault();
+
+			if (this.regFormAuth.style.display = 'none') {
+				this.createFormAcc.style.display = 'flex';
+			}
+
+		})
+	}
+	getRedirectAcc() {
+
+		this.createLinkAcc.addEventListener('click', (e) => {
+			e.preventDefault();
+
+			if (this.createFormAcc.style.display = 'none') {
+				this.regFormAuth.style.display = 'flex';
+			}
+
+		})
+	}
 
 	validLogin(login) {
 		let re = /^[a-z0-9_-]{5,16}$/;
@@ -24,37 +80,73 @@ class Authorization {
 		return rePass.test(String(password));
 	}
 
-	createAcc() {
+	getAuth() {
 		this.form.addEventListener('submit', (e) => {
-			let emptyInputs = Array.from(this.input).filter(item => item.value === '');
+			e.preventDefault();
+			this.checkInputs();
 
-			// e.preventDefault();
-
-			this.input.forEach(function (elem) {
-				if (elem.value === '') {
-					elem.classList.add('error');
-				} else {
-					elem.classList.remove('error');
-				}
-			})
-
-			if (emptyInputs.length !== 0) {
-				console.log('inputs not field')
-				return false;
-			}
-
-			if (!this.validLogin(this.login.value)) {
-				this.login.classList.add('error');
-				return false;
-			};
-
-			if (!this.validPassword(this.password.value)) {
-				this.password.classList.add('error');
-				return false;
-			};
+			window.location.href = "index.html";
 
 		})
 
+	}
+	createAcc() {
+		this.form.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			this.checkInputs();
+
+			if (this.createPass.value !== this.passwordConfirm.value) {
+				this.passwordConfirm.classList.add('error');
+				return false;
+			}
+
+			this.showPassword();
+
+			window.location.href = "index.html";
+
+
+		})
+	}
+	showPassword() {
+
+		this.checkPass.addEventListener('click', () => {
+			// e.preventDefault();
+			if (this.createPass.getAttribute('type') === 'password' && this.passwordConfirm.getAttribute('type') === 'password') {
+				this.createPass.setAttribute('type', 'text');
+				this.passwordConfirm.setAttribute('type', 'text');
+			} else {
+				this.createPass.setAttribute('type', 'password');
+				this.passwordConfirm.setAttribute('type', 'password');
+
+			}
+		});
+	}
+
+	checkInputs() {
+		let emptyInputs = Array.from(this.input).filter(item => item.value === '');
+		this.input.forEach(function (elem) {
+			if (elem.value === '') {
+				elem.classList.add('error');
+			} else {
+				elem.classList.remove('error');
+			}
+		})
+
+		if (emptyInputs.length !== 0) {
+			console.log('inputs not field')
+			return false;
+		}
+
+		if (!this.validLogin(this.login.value)) {
+			this.login.classList.add('error');
+			return false;
+		};
+
+		if (!this.validPassword(this.password.value)) {
+			this.password.classList.add('error');
+			return false;
+		};
 	}
 }
 
@@ -62,10 +154,21 @@ class Authorization {
 const reg = new Authorization({
 	input: 'input',
 	form: '.reg-form',
+	// authorization
+	regFormAuth: '#reg-form-auth',
 	login: '#reg-login',
 	password: '#reg-password',
-	button: '.reg-btn'
-})
+	btnSubmit: '#reg-submit',
+	regLinkAuth: '#reg-link-acc',
+	// create new acc
+	createFormAcc: '#create-form-acc',
+	createPass: '#create-password',
+	passwordConfirm: '#create-confirm',
+	checkPass: '#create-check',
+	btnCreate: '#create-create-btn',
+	createLinkAcc: '#create-link-acc',
+
+});
 
 reg.init();
 
